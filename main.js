f5c8079198a54bad3c8ccb03b498fc80c5a1ae1c
Kendall <kendallawills@gmail.com>
2023-07-14 11:05:07 -0400

    Let

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ var favoriteFood = 'pizza';
 var numOfSlices = 8;
 console.log(favoriteFood);
 console.log(numOfSlices);
+
+//Create a Variable Let
+let changeMe = true;
+changeMe = false;
+console.log(changeMe);
