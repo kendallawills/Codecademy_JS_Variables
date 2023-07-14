13e8b6b253a569a4281e6ad2cd134abc353b5a8b
Kendall <kendallawills@gmail.com>
2023-07-14 11:27:10 -0400

    Math Assignment Operators

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ console.log(changeMe);
 //Create a Variable Const
 const entree = 'Enchiladas';
 console.log(entree);
+
+//Math Assignment Operators
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+console.log('The value of levelUp:', levelUp); 
+console.log('The value of powerLevel:', powerLevel); 
+console.log('The value of multiplyMe:', multiplyMe); 
+console.log('The value of quarterMe:', quarterMe);
